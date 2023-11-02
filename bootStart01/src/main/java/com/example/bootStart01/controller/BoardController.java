45b0ac8e2c92651d601096af5c5fd121b80aac8e
진영준 <jinange9@gmail.com>
2023-11-02 19:49:18 +0900

    com
    com

--- bootStart01/src/main/java/com/example/bootStart01/controller/BoardController.java
+++ bootStart01/src/main/java/com/example/bootStart01/controller/BoardController.java
@@ -3,11 +3,14 @@ package com.example.bootStart01.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.bootStart01.entity.Board;
 import com.example.bootStart01.repository.BoardRepository;
+import com.example.bootStart01.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,12 +19,24 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class BoardController {
 
-	private final BoardRepository boardRepository;
+	//private final BoardRepository boardRepository;
+	private final BoardService boardService;
 	
+	// 게시글 조회
 	@GetMapping("/main")
-	public String goBoardMain() {
-		List<Board> boardList = this.boardRepository.findAll();
+	public String goBoardMain(Model model) {
+		List<Board> boardList = this.boardService.selectList();
 		// findAll = select * from board 와 같은 의미
+		model.addAttribute(boardList);
 		return "board_main";
+	}
+	
+	// 게시글 상세조회
+	@GetMapping("/detail/{boardNum}")
+	public String boardDetail(Model model, @PathVariable("boardNum") Integer boardNum) {
+		Board board = this.boardService.boardDetail(boardNum);
+		model.addAttribute(board);
+		
+		return "board_detail";
 	}
 }
